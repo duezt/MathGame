9b64f5bf176d16ca8021c16d551129c48ef7b6fb
duezt <klkrdcf@gmail.com>
2024-09-15 12:15:16 -0400

    Adding the game methods

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,1 +1,210 @@
 ﻿
+
+
+
+//Variables
+using System.Numerics;
+
+string name = "";
+DateTime date = DateTime.Now;
+string option = "";
+
+
+//Display menu and user name's input
+Console.WriteLine("What is your name?");
+name = Console.ReadLine();
+Console.WriteLine($@"Welcome to the math game {name}, today is {date.DayOfWeek}
+Please select one of the following games option:
+A - Addition 
+S - Substraction 
+M - Multiplication
+D - Division");
+
+
+//User input for options and logic
+
+option = Console.ReadLine().ToLower().Trim();
+
+switch (option)
+{
+    case "a":
+        AdditionGame();
+        break;
+    case "s":
+        SubtractionGame();
+        break;
+    case "m":
+        MultiplicationGame();
+        break;
+    case "d":
+        Console.WriteLine("D worked");
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
+
+
+void AdditionGame()
+{
+
+
+
+    var rand = new Random();
+    
+    int correctAnwsercounter = 0;
+    int IncorrectAwnserCounter = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        bool isNumber = false;
+
+        int firstNumber = rand.Next(1, 100);
+        int secondNumber = rand.Next(1, 100);
+        int operationResults = firstNumber + secondNumber;
+        Console.WriteLine($"{firstNumber} + {secondNumber} = ?");
+
+
+        while (isNumber == false) 
+        {
+            string userInput = Console.ReadLine().Trim();
+            int userAnwser;
+
+            if (int.TryParse(userInput, out userAnwser))
+            {
+
+
+
+                if (userAnwser == operationResults)
+                {
+                    Console.WriteLine($"You're anwser is correct! {firstNumber} + {secondNumber} = {operationResults}");
+                    correctAnwsercounter++;
+                }
+                else
+                {
+                    Console.WriteLine($"You're anwser is incorrect! the correct anwser is {operationResults} and you're answer was {userAnwser}");
+                    IncorrectAwnserCounter++;
+                }
+
+                isNumber = true;
+
+            }
+            else
+            {
+                Console.WriteLine("Please, type in a number not letters");
+
+
+            }
+        }
+    }
+}
+
+
+
+void SubtractionGame()
+{
+
+    var rand = new Random();
+
+    int correctAnwsercounter = 0;
+    int IncorrectAwnserCounter = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        bool isNumber = false;
+
+        int firstNumber = rand.Next(1, 100);
+        int secondNumber = rand.Next(1, 100);
+        int operationResults = firstNumber - secondNumber;
+        Console.WriteLine($"{firstNumber} - {secondNumber} = ?");
+
+
+        while (isNumber == false)
+        {
+            string userInput = Console.ReadLine().Trim();
+            int userAnwser;
+
+            if (int.TryParse(userInput, out userAnwser))
+            {
+
+
+
+                if (userAnwser == operationResults)
+                {
+                    Console.WriteLine($"You're anwser is correct! {firstNumber} - {secondNumber} = {operationResults}");
+                    correctAnwsercounter++;
+                }
+                else
+                {
+                    Console.WriteLine($"You're anwser is incorrect! the correct anwser is {operationResults} and you're answer was {userAnwser}");
+                    IncorrectAwnserCounter++;
+                }
+
+                isNumber = true;
+
+            }
+            else
+            {
+                Console.WriteLine("Please, type a number not letters");
+
+
+            }
+        }
+    }
+}
+
+
+void MultiplicationGame()
+{
+
+    var rand = new Random();
+
+    int correctAnwsercounter = 0;
+    int IncorrectAwnserCounter = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        bool isNumber = false;
+
+        int firstNumber = rand.Next(1, 100);
+        int secondNumber = rand.Next(1, 100);
+        int operationResults = firstNumber * secondNumber;
+        Console.WriteLine($"{firstNumber} x {secondNumber} = ?");
+
+
+        while (isNumber == false)
+        {
+            string userInput = Console.ReadLine().Trim();
+            int userAnwser;
+
+            if (int.TryParse(userInput, out userAnwser))
+            {
+
+
+
+                if (userAnwser == operationResults)
+                {
+                    Console.WriteLine($"You're anwser is correct! {firstNumber} x {secondNumber} = {operationResults}");
+                    correctAnwsercounter++;
+                }
+                else
+                {
+                    Console.WriteLine($"You're anwser is incorrect! the correct anwser is {operationResults} and you're answer was {userAnwser}");
+                    IncorrectAwnserCounter++;
+                }
+
+                isNumber = true;
+
+            }
+            else
+            {
+                Console.WriteLine("Please, type a number not letters");
+
+
+            }
+        }
+    }
+}
+
+
+
