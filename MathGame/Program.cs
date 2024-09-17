a9e21264e98e195b7c3830d9efadb2b3a2606736
duezt <klkrdcf@gmail.com>
2024-09-16 20:56:38 -0400

    Division game made.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,9 +1,9 @@
-﻿
-
+﻿using MathGame;
+using System.Numerics;
 
 
 //Variables
-using System.Numerics;
+
 
 string name = "";
 DateTime date = DateTime.Now;
@@ -37,7 +37,7 @@ switch (option)
         MultiplicationGame();
         break;
     case "d":
-        Console.WriteLine("D worked");
+        DivisionGame();
         break;
     default:
         Console.WriteLine("Invalid input");
@@ -208,3 +208,58 @@ void MultiplicationGame()
 
 
 
+void DivisionGame()
+{
+
+
+
+    var rand = new Random();
+
+    int correctAnwsercounter = 0;
+    int IncorrectAwnserCounter = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        bool isNumber = false;
+        int firstNumber = rand.Next(1, 25);
+        int secondNumber = rand.Next(1, 100);
+
+        //This variable would give always a divend that will give a intenger result.
+        int dividend = firstNumber * secondNumber;
+        int operationResults =  dividend / firstNumber;
+        Console.WriteLine($"{dividend} / {firstNumber} = ?");
+
+
+        while (isNumber == false)
+        {
+            string userInput = Console.ReadLine().Trim();
+            int userAnwser;
+
+            if (int.TryParse(userInput, out userAnwser))
+            {
+
+
+
+                if (userAnwser == operationResults)
+                {
+                    Console.WriteLine($"You're anwser is correct! {firstNumber} / {secondNumber} = {operationResults}");
+                    correctAnwsercounter++;
+                }
+                else
+                {
+                    Console.WriteLine($"You're anwser is incorrect! the correct anwser is {operationResults} and you're answer was {userAnwser}");
+                    IncorrectAwnserCounter++;
+                }
+
+                isNumber = true;
+
+            }
+            else
+            {
+                Console.WriteLine("Please, type in a number not letters");
+
+
+            }
+        }
+    }
+}
